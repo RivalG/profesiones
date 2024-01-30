6dffce2c492394c9738a6dfcde06d792dddd6be4
RIVALDO <rivaldo@UBUNTU.myguest.virtualbox.org>
2024-01-30 13:34:53 -0500

    Agregado archivo node

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const config = require('./config');
 
 // Ajusta la ruta según la nueva estructura
 const clientes = require('./modulos/clientes/rutas');
 
+
 const app = express();
 const router = express.Router();  // Agrega esta línea para crear el router
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());  // Agrega esta línea para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.urlencoded({extended: true}));
 
@@ -20,8 +23,5 @@ app.use('/api/clientes', clientes(router));  // Pasa el router como parámetro
 
 // Agrega esta línea para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('public'));
-
-// Elimina la línea siguiente, ya que la ruta '/api/clientes' ya está en uso
-// app.use('/api/clientes', clientes(router));
 
 module.exports = app;
